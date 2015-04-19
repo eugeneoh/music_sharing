abc1d988ff8090cdc6a8776e36c94be4d82fc869
Eugene Oh <eugeneoh15@gmail.com>
2015-04-18 20:14:44 -0400

    update to 0.1.3

--- test.js
+++ test.js
@@ -110,7 +110,7 @@ $(document).ready(function() {
 		console.log("getPlaylists called");
 		pod.query()
 			.filter({
-				_owner: "http://music.fakepods.com",
+				_owner: "http://music.databox1.com",
 				isPlaylist: true
 			})
 			.onAllResults(function(items) {
