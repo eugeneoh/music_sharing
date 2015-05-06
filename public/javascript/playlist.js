2045dba47f048187544bb7cab49b93fc43de03d7
Eugene Oh <eugeneoh15@gmail.com>
2015-05-06 15:06:36 -0400

    trying query params

--- public/javascript/playlist.js
+++ public/javascript/playlist.js
@@ -1,7 +1,90 @@
 $(document).ready(function() {
+	var pod = crosscloud.connect();
 	console.log(window.location.href);
 	var queryString = window.location.href.split('?')[1];
 	console.log(queryString);
 	var id = queryString.split('=')[1];
 	console.log(id);
+	pod.onLogin(function(userID) {
+		console.log(userID);
+		currentID = userID;
+		getSongsInPlaylist(id);
+	});
+
+	function getSongsInPlaylist(id) {
+		pod.query()
+			.filter({
+				_id: id
+			})
+			.onAllResults(function(items) {
+				displaySongsInPlaylist(items[0]);
+			}).start();
+	}
+
+	function displaySongsInPlaylist(item) {
+		songs.empty();
+		currentPlaylistSongs = item.songs;
+		console.log(currentPlaylistSongs);
+		for (i in currentPlaylistSongs) {
+			var song = currentPlaylistSongs[i];
+			var songListItem = $('<li>', {
+				text: song.name,
+				id: song.videoId,
+				class: "list-group-item"
+			});
+			songListItem.data('song-order-number', i);
+			playQueue.push(song);
+			songListItem.click(function(e) {
+				queueList.empty();
+				initializePlayQueue();
+				player.loadVideoById(e.target.id);
+				// playQueue.shift();
+				var tmp = [];
+				for (var i = 0; i < $(e.target).data('song-order-number'); i++) {
+					tmp.push(playQueue.shift());
+				}
+				console.log('tmp is: ', tmp);
+				for (var j = 0; j < tmp.length; j++) {
+					playQueue.push(tmp[j]);
+				}
+				for (var z = 0; z < playQueue.length; z++) {
+					var queueListCtn = $('<li>');
+					var queueListItem = $('<a>', {
+						text: playQueue[z].name,
+						id: playQueue[z].videoId
+					});
+					queueListItem.data('song-queue-number', z);
+					queueListItem.click(function(e) {
+						songQueueNumber = $(e.target).data('song-queue-number');
+						console.log(e.target);
+						player.loadVideoById(playQueue[songQueueNumber].videoId);
+						playQueue = playQueue.splice(songQueueNumber);
+						console.log(playQueue);
+						$(queueList.children()[songQueueNumber]).remove();
+						for (var i = songQueueNumber; i < queueList.children().length; i++) {
+							console.log(i);
+							$($(queueList.children()[i]).children()[0]).data('song-queue-number', i);
+						}
+						console.log(queueList.children());
+					});
+					queueListCtn.append(queueListItem);
+					queueListCtn.appendTo(queueList);
+				}
+				console.log('playQueue is: ', playQueue);
+			});
+			songListItem.appendTo(songs);
+		}
+		// for (z in playQueue) {
+		// 	var queueListCtn = $('<li>');
+		// 	var queueListItem = $('<a>', {
+		// 		text: playQueue[z].name,
+		// 		id: playQueue[z].videoId
+		// 	});
+		// 	queueListCtn.click(function(e) {
+
+		// 	});
+		// 	queueListCtn.append(queueListItem);
+		// 	queueListCtn.appendTo(queueList);
+		// }
+	}
 });
